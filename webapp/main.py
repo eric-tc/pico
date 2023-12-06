0286c9b92d54fd74ad8277aab7863c2ff744f537
eric.tondelli <eric.tondelli@gmail.com>
2023-12-06 18:16:48 +0100

    update pathology path

--- webapp/main.py
+++ webapp/main.py
@@ -1,6 +1,6 @@
 # main.py
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template,request
 from flask_login import login_required, current_user
 
 main = Blueprint('main', __name__)
@@ -17,3 +17,32 @@ def index():
 @login_required
 def profile():
     return render_template('profile.html', name=current_user.name)
+
+
+@main.route('/anagrafica')
+@login_required
+def anagrafica():
+    return render_template('anagrafica.html', id=current_user.id)
+
+
+@main.route('/pathology')
+@login_required
+def patology():
+
+    if request.method == 'POST':
+        # Extract form data from the request
+        nprs_vas = request.form.get('nprs_vas')
+        prom_arom_mcpj = request.form.get('prom_arom_mcpj')
+        prom_arom_Ipj = request.form.get('prom_arom_Ipj')
+        abduction = request.form.get('abduction')
+        anterposition = request.form.get('anterposition')
+        kapandji = request.form.get('kapandji')
+        pinch = request.form.get('pinch')
+        grip = request.form.get('grip')
+        dash = request.form.get('dash')
+        prwhe = request.form.get('prwhe')
+        Eaton_littler = request.form.get('Eaton_littler')
+        scar_status = request.form.get('scar_status')
+        scar_type = request.form.get('scar_type')
+
+    return render_template('patology.html')
--- webapp/models.py
+++ webapp/models.py
@@ -2,9 +2,57 @@
 
 from flask_login import UserMixin
 from . import db
+from datetime import datetime
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
+    name = db.Column(db.String(120))
+    role = db.Column(db.Integer)
+
+def create_phatology_data():
+
+    existing_data= Phatology.query.first()
+    if not existing_data:
+        #Create all Pathology
+        rizoartrosi= Phatology(name="Rizoartrosi")
+
+
+
+
+class Phatology(db.Model):
+
+    id = db.Column(db.Integer,primary_key=True)
+    name = db.Column(db.String(120))
+
+"""
+This class will contains all data releated to
+doctor patients and all value from rizoartrosi based on
+different timestamp
+
+"""
+class Rizoartrosi(db.Model):
+
+    id = db.Column(db.Integer,primary_key=True)
+    doctor = db.Column(db.Integer)
+    patient = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    #Parametri
+    nprs_vas=db.Column(db.Integer)
+    prom_arom_mcpj= db.Column(db.Integer)
+    prom_arom_Ipj=db.Column(db.Integer)
+    abduction=db.Column(db.Integer)
+    anterposition=db.Column(db.Integer)
+    kapandji=db.Column(db.Integer)
+    pinch=db.Column(db.Integer)
+    grip=db.Column(db.Integer)
+    dash=db.Column(db.Integer)
+    prwhe=db.Column(db.Integer)
+    Eaton_littler=db.Column(db.Integer)
+    scar_status= db.Column(db.String(100))
+    scar_type = db.Column(db.String(100))
+    
+
+    
+
