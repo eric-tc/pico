3175ef683b82357be1526aba198d0e6c926ddef7
eric.tondelli <eric.tondelli@gmail.com>
2023-12-18 12:46:02 +0100

    gestione notifiche paziente

--- web/app/patient.py
+++ web/app/patient.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template,request,jsonify
 from flask_login import login_required, current_user
 from .internal_data import ROLE,NOTIFICATION_STATUS
 from .models import User,DoctorPatient,Notification
+from sqlalchemy import or_, and_
 from . import db
 
 patient = Blueprint('patient', __name__)
@@ -13,7 +14,15 @@ def profile_patient():
 
     #get all notification
 
-    current_notifications_list= Notification.query.filter((Notification.id_patient==current_user.id)).all()
+    current_notifications_list= Notification.query.filter(
+        and_(
+            Notification.id_patient==current_user.id,
+            or_(
+                Notification.status==NOTIFICATION_STATUS.SENT.value,
+                Notification.status==NOTIFICATION_STATUS.APPROVED.value
+            )
+        )
+    ).all()
 
     print(len(current_notifications_list))
 
@@ -48,6 +57,42 @@ def accept_doctor_request():
         else:
             
             return jsonify({'error': 'User not found'}), 404
+    except Exception as e:
+        db.session.rollback()
+        print(e)
+        return jsonify({'error': str(e)}), 500
+    finally:
+        db.session.close()
+
+
+"""
+Ajax request to link doctor and patient
+"""
+@patient.route('/remove_doctor_request',methods=["POST"])
+@login_required
+def remove_doctor_request():
+
+    
+    id_notification= request.json.get('id_notification')
+    id_doctor = request.json.get('id_doctor')
+
+    try:
+        # Create a new link when patient confirm doctor requests
+        #update notification status
+        notification_to_update= Notification.query.get(id_notification)
+        removeLinkPatientDoctor = DoctorPatient.query.filter_by(id_doctor=id_doctor, id_patient=current_user.id).first()
+
+        if notification_to_update:
+
+            notification_to_update.status= NOTIFICATION_STATUS.TOELIMINATE.value
+            db.session.delete(removeLinkPatientDoctor)
+            db.session.commit()
+            
+            return jsonify({"success":"rimosso"})
+
+        else:
+            
+            return jsonify({'error': 'notification not found'}), 404
     except Exception as e:
         db.session.rollback()
         print(e)
