7ebc98d92dec8f385c4024921a493f216c614a06
eric.tondelli <eric.tondelli@gmail.com>
2025-01-06 16:28:24 +0100

    aggiornamento gestione migrazioni con database db_dev per development

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -70,8 +70,9 @@ def create_app():
     from .models import User
     
     print("CREATE DB")
-    with app.app_context():
-        db.create_all()
+    #Handled with migration command DO NOT USE
+    # with app.app_context():
+    #     db.create_all()
     
     @login_manager.user_loader
     def load_user(user_id):
